6679bf9f2b067f292711065d613faa6c2504dca3
mykyta.chepurko <mykyta.chepurko@dev.pro>
2022-05-18 21:43:13 +0300

    Add file with tasks

---
+++ test1.py
@@ -0,0 +1,9 @@
+# Write a function which return list of even numbers
+# Example: 
+# arr = [1,2,3,4,5,"Jack", "John", "Peter"]
+# result = [2,4]
+
+
+
+# Using 'requests' lib make a GET request and print response's body
+# Site for testing - https://randomuser.me/
