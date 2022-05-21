5a5975bf99800f5b36ca800a02e9fdb00188d734
dasha <bezhnard@gmail.com>
2022-05-21 13:10:49 +0300

    Solve task 1

--- test1.py
+++ test1.py
@@ -7,3 +7,20 @@
 
 # Using 'requests' lib make a GET request and print response's body
 # Site for testing - https://randomuser.me/
+
+
+arr = [1,2,3,4,5,"Jack", "John", "Peter"]
+
+
+def even_nbrs(x):
+    result = []
+
+    for i in x:
+        if isinstance(i, int):
+            if i % 2 == 0:
+                result.append(i)
+
+    return result
+
+print(even_nbrs(arr))
+
