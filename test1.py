bfaf341e5ca4fe427230711c5a25325790d0cb00
dasha <bezhnard@gmail.com>
2022-05-23 22:50:28 +0300

    Add docstring to even_nbrs

--- test1.py
+++ test1.py
@@ -12,10 +12,15 @@
 arr = [1,2,3,4,5,"Jack", "John", "Peter"]
 
 
-def even_nbrs(x):
+def even_nbrs(array):
+    """
+    The function filters passed list and saves only even numbers.
+    :param array: list List may include any data types
+    :return: list List includes only even numbers
+    """
     result = []
 
-    for i in x:
+    for i in array:
         if isinstance(i, int):
             if i % 2 == 0:
                 result.append(i)
